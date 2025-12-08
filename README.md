084fd79a6bc621d245c32fd4ce88975b44115b1d
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-12-08 10:35:34 -0500

    Refactoring - naming change

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Spotify API app Angular FE
 
 https://xomify.com - email dominickj.giordano@gmail.com to get whitelisted (dev mode)
 
-* FE Repo: https://github.com/domgiordano/angular-spotify
-* BE Repo: https://github.com/domgiordano/python-spotify
-* Infra Repo: https://github.com/domgiordano/angular-spotify-infrastructure
-* Terraform Workspace: https://app.terraform.io/app/Domjgiordano/workspaces/angular-spotify-infrastructure
+- FE Repo: https://github.com/domgiordano/xomify-frontend
+- BE Repo: https://github.com/domgiordano/xomify-backend
+- Infra Repo: https://github.com/domgiordano/xomify-frontend-infrastructure
+- Terraform Workspace: https://app.terraform.io/app/Domjgiordano/workspaces/xomify-frontend-infrastructure
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,21 +10,20 @@ import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
   showDynamicButton: boolean = false;
   footerButtonText: string = '';
-  githubRepoUrl: string = 'https://github.com/domgiordano/angular-spotify';
+  githubRepoUrl: string = 'https://github.com/domgiordano/xomify-frontend';
   userId: string;
-
 
   constructor(
     private router: Router,
     private SongService: SongService,
     private PlaylistService: PlaylistService,
     private UserService: UserService,
-    private ToastService: ToastService,
+    private ToastService: ToastService
   ) {}
 
   ngOnInit() {
@@ -45,14 +44,19 @@ export class FooterComponent implements OnInit {
     });
   }
 
-  private uploadImage$(playlistId: string, base64Image: string): Observable<any> {
-    return new Observable(observer => {
+  private uploadImage$(
+    playlistId: string,
+    base64Image: string
+  ): Observable<any> {
+    return new Observable((observer) => {
       // Extract the base64 string if it includes the data URL prefix
       //const base64String = base64Image.split(',')[1];
       const base64String = base64Image.replace(/\n/g, '');
 
-
-      this.PlaylistService.uploadPlaylistImage(playlistId, base64String).subscribe({
+      this.PlaylistService.uploadPlaylistImage(
+        playlistId,
+        base64String
+      ).subscribe({
         next: (data) => {
           observer.next(data);
           observer.complete();
@@ -89,45 +93,67 @@ export class FooterComponent implements OnInit {
         playlistName = `Last Year Top Songs: ${formattedDate}`;
       }
 
-      let currentTopSongsUriList: string[] = currentTopSongs.map(track => track.uri);
+      let currentTopSongsUriList: string[] = currentTopSongs.map(
+        (track) => track.uri
+      );
       this.userId = this.UserService.getUserId();
-      this.PlaylistService.createPlaylist(this.userId, playlistName, playlistDesc).pipe(take(1)).subscribe({
-        next: playlist => {
-          this.PlaylistService.addPlaylistSongs(playlist, currentTopSongsUriList).pipe(take(1)).subscribe({
-            next: data => {
-              console.log(data);
-              // Now upload the image
-              this.uploadImage$(playlist.id, environment.logoBase64).subscribe({
-                next: (imageData) => {
-                  console.log('Image uploaded successfully:', imageData);
+      this.PlaylistService.createPlaylist(
+        this.userId,
+        playlistName,
+        playlistDesc
+      )
+        .pipe(take(1))
+        .subscribe({
+          next: (playlist) => {
+            this.PlaylistService.addPlaylistSongs(
+              playlist,
+              currentTopSongsUriList
+            )
+              .pipe(take(1))
+              .subscribe({
+                next: (data) => {
+                  console.log(data);
+                  // Now upload the image
+                  this.uploadImage$(
+                    playlist.id,
+                    environment.logoBase64
+                  ).subscribe({
+                    next: (imageData) => {
+                      console.log('Image uploaded successfully:', imageData);
+                    },
+                    error: (err) => {
+                      console.error('Error uploading image:', err);
+                      this.ToastService.showNegativeToast(
+                        'Error uploading playlist image'
+                      );
+                    },
+                    complete: () => {
+                      console.log('Image upload complete.');
+                    },
+                  });
                 },
                 error: (err) => {
-                  console.error('Error uploading image:', err);
-                  this.ToastService.showNegativeToast('Error uploading playlist image');
+                  console.error('Error Adding Items to Playlist', err);
+                  this.ToastService.showNegativeToast(
+                    'Error adding songs to playlist'
+                  );
                 },
                 complete: () => {
-                  console.log("Image upload complete.");
-                }
+                  console.log('Adding Items to Playlist Complete.');
+                  this.ToastService.showPositiveToast(
+                    'Playlist successfully added to your Spotify account!'
+                  );
+                },
               });
-            },
-            error: err => {
-              console.error('Error Adding Items to Playlist', err);
-              this.ToastService.showNegativeToast('Error adding songs to playlist');
-            },
-            complete: () => {
-              console.log("Adding Items to Playlist Complete.");
-              this.ToastService.showPositiveToast('Playlist successfully added to your Spotify account!');
-            },
-          });
-        },
-        error: err => {
-          console.error('Error Creating Playlist', err);
-          this.ToastService.showNegativeToast('Error creating playlist');
-        },
-        complete: () => {
-          console.log('Playlist Created.');
-        }
-      });
+          },
+          error: (err) => {
+            console.error('Error Creating Playlist', err);
+            this.ToastService.showNegativeToast('Error creating playlist');
+          },
+          complete: () => {
+            console.log('Playlist Created.');
+          },
+        });
     }
   }
 
